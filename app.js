e6a0d23a3ed6aa881337286c7ae1609201a8ffe8
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-08 09:31:52 +0300

    java-assignment2

---
+++ app.js
@@ -0,0 +1,21 @@
+// first-question
+let favorColor = "blue";
+let = bestFriend = "hassan";
+let = country = "kenya";
+
+// second-question
+console.log("my favorite color is" + " " + favorColor + ", my best friend is" + " " + bestFriend + ",and i live in" + " " + country );
+
+// third-question
+console.log(favorColor.length);
+console.log(bestFriend.length);
+console.log(country.length);
+
+// fourth-question
+console.log(favorColor.toUpperCase());
+console.log(bestFriend.toLowerCase());
+console.log(country.slice(0,4));
+
+// fifth-question
+let userName = prompt ("whats your name?");
+console.log("Hello," + userName + "Nice to meet you" );
